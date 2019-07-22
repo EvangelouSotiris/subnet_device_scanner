86a84c5e580662d437065e09727774154dc397e2
EvangelouSotiris <swtevag@gmail.com>
2019-07-22 13:59:46 +0300

    add colour, more readable anduser-friendly

--- net_discoverer.py
+++ net_discoverer.py
@@ -3,44 +3,60 @@ import json
 import sys
 import socket
 
+LightGreen = "\x1b[92m"
+LightRed = "\x1b[91m"
+Default = "\x1b[39m"
+Cyan = "\x1b[96m"
+Magenta = "\x1b[95m"
+Black = "\x1b[30m"
+Blue = "\x1b[94m"
+B_Default = "\x1b[49m"
+B_White = "\x1b[107m"
+
 DEFAULT_CIDR=24
 local_ip = socket.gethostbyname(socket.gethostname())
 if DEFAULT_CIDR == 24:
     net_id = local_ip.split('.')[0]+'.'+local_ip.split('.')[1]+'.'+local_ip.split('.')[2]+'.0/24'
 
-print('Searching devices in the network: ' +  net_id)
+print('Searching devices in the network: ' +  net_id )
 nm = nmap.PortScanner()
 nm.scan(hosts=net_id, arguments='-sP')
 nm2 = nmap.PortScanner()
 for host in nm.all_hosts():
-    [print('-', end="") for i in range(len(host))]
-    print('\n'+host)
-    [print('-', end="") for i in range(len(host))]
-    print()
     result = nm2.scan(hosts=host, arguments='-sV -O -v')
+    print(Blue)
+    [print('-', end="") for i in range(len(host))]
+    print('\n'+B_White + Black + host + B_Default + Blue)
+    [print('-', end="") for i in range(len(host))]
+    print(Default) 
     main = result['scan'][host]
     del result['nmap']
     del main['portused']
     #print(json.dumps(result, indent=4))
-    print('Hostname= '+ main['hostnames'][0]['name'])
+    if main['hostnames'][0]['name'] != '':
+        print('Hostname= '+ main['hostnames'][0]['name'])
     if "mac" in main['addresses']:
         mac = main['addresses']['mac']
         print('MAC address= ' + mac)
         if len(main['vendor'])!=0:
             print('Vendor= ' + main['vendor'][mac])
-    print('State= ' + main['status']['state'])
+    print('State= '+ LightGreen + main['status']['state'] + Default)
     if 'uptime' in main:
         print('Uptime= '+ str(int(main['uptime']['seconds'])/60) + ' minutes')
     print('TCP Services:')
     for port in main['tcp']:
-        print('|-' + main['tcp'][port]['name'], end="")
+        print('|-'+ Cyan + main['tcp'][port]['name'] + Default, end="")
         if main['tcp'][port]['version'] != '':
             print('(v. ' + main['tcp'][port]['version']+ ')', end ="")
-        print(' at port ' + str(port) + ' - ' + main['tcp'][port]['state'], end ="")
+        print(' at port ' + str(port) + ' - ', end="")
+        if main['tcp'][port]['state'] == 'open':
+            print(LightGreen + main['tcp'][port]['state'] + Default, end ="")
+        else:
+            print(LightRed + main['tcp'][port]['state'] + Default, end ="")
         if main['tcp'][port]['product'] != "":
             print( ' - Product: ' + main['tcp'][port]['product'])
         else: print()
-    print('Operating System= ' + main['osmatch'][0]['name'])
+    print('Operating System= ' + Magenta + main['osmatch'][0]['name'] + Default)
     print('Common Platform Enumeration:')
     for os in range(len(main['osmatch'][0]['osclass'])):
         print('|-' + main['osmatch'][0]['osclass'][os]['type'] + ' - ' + main['osmatch'][0]['osclass'][os]['cpe'][0])
